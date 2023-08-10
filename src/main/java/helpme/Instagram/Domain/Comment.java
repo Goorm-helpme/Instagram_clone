2934b1ecdc04a8a01e81cd052d320430db5ac1e2
JaeMinKim <kjaemin0122@naver.com>
2023-08-10 14:48:46 +0900

    Update Comment.java
    CASCADE DELETE

--- src/main/java/helpme/Instagram/Domain/Comment.java
+++ src/main/java/helpme/Instagram/Domain/Comment.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,7 +23,6 @@ public class Comment {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "peed_id")
-    @OnDelete(action = OnDeleteAction.CASCADE)
     private Peed peed;
 
     @OneToMany(mappedBy = "comment")
