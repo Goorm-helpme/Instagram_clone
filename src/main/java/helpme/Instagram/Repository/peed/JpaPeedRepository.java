329c27ff12b0042d9377f72f587a8d88f42b8ee1
JinhwanB <paewhal20@gmail.com>
2023-08-16 19:10:58 +0900

    cors 설정

--- src/main/java/helpme/Instagram/Repository/peed/JpaPeedRepository.java
+++ src/main/java/helpme/Instagram/Repository/peed/JpaPeedRepository.java
@@ -5,7 +5,6 @@ import helpme.Instagram.Repository.QueryDsl.PeedRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
